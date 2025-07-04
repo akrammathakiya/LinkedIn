f6371e2088a598fd21a40cb39671bf3c9821ce6a
akrammathakiya <mathakiyaakram@gmail.com>
2025-07-04 09:30:56 +0530

    integrate signup api and allow cors middleware in backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,18 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 dotenv.config();
 const port = process.env.PORT || 8000
 const app = express();
+
+
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
 
 app.use("/api/user",userRouter)
 
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import logo from "../assets/logo.svg";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
+import { authDataContext } from "../context/AuthContext";
+import axios from "axios"
 
 const Signup = () => {
-   let [show,setShow] = useState(false)
-   const navigate = useNavigate()
+  let [show, setShow] = useState(false);
+  let { serverUrl } = useContext(authDataContext);
+  const navigate = useNavigate();
+
+  let [firstName, setFirstName] = useState("");
+  let [lastName, setLastName] = useState("");
+  let [userName, setUserName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault()
+    try {
+      let result = await axios.post(serverUrl + "/api/user/signup", {
+        firstName,
+        lastName,
+        userName,
+        email,
+        password
+      },{withCredentials:true});
+      console.log(result);
+    } catch (error) {
+      console.log(error);  
+    }
+  };
+
   return (
     <div className="w-full h-screen bg-[white] flex flex-col items-center justify-start gap-[10px]">
       <div className="p-[35px] w-full flex items-center">
         <img src={logo} alt="" className="w-28 h-auto" />
       </div>
-      <form className="w-[90%] max-w-[400px] md:shadow-xl flex flex-col justify-center gap-[20px] p-[15px]">
+      <form className="w-[90%] max-w-[400px] md:shadow-xl flex flex-col justify-center gap-[20px] p-[15px]" onSubmit={handleSignup}>
         <h1 className="text-gray-800 text-[30px] font-semibold mb-[30px]">
           Signup
         </h1>
@@ -20,6 +46,8 @@ const Signup = () => {
           placeholder="FirstName"
           required
           className="border-2 border-gray-600 text-gray-800 text-[18px] px-[12px] py-[10px] rounded focus:outline-none focus:placeholder-transparent transition-all duration-200"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
         />
 
         <input
@@ -27,6 +55,8 @@ const Signup = () => {
           placeholder="LastName"
           required
           className="border-2 border-gray-600 text-gray-800 text-[18px] px-[12px] py-[10px] rounded focus:outline-none focus:placeholder-transparent transition-all duration-200"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
         />
 
         <input
@@ -34,6 +64,8 @@ const Signup = () => {
           placeholder="UserName"
           required
           className="border-2 border-gray-600 text-gray-800 text-[18px] px-[12px] py-[10px] rounded focus:outline-none focus:placeholder-transparent transition-all duration-200"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
         />
 
         <input
@@ -41,23 +73,38 @@ const Signup = () => {
           placeholder="Email"
           required
           className="border-2 border-gray-600 text-gray-800 text-[18px] px-[12px] py-[10px] rounded focus:outline-none focus:placeholder-transparent transition-all duration-200"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <div className="border-2 border-gray-600 text-gray-800 text-[18px] px-[12px] py-[10px] rounded relative">
           <input
-            type={show?"text":"password"}
+            type={show ? "text" : "password"}
             placeholder="Password"
             required
             className="w-full bg-transparent outline-none border-none focus:placeholder-transparent transition-all duration-200"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <span className="absolute right-3 text-[#0A66C2] cursor-pointer"  onClick={()=>{
-            setShow(!show)
-          }}>{show?"Hidden":"Show"}</span>
+          <span
+            className="absolute right-3 text-[#0A66C2] cursor-pointer"
+            onClick={() => {
+              setShow(!show);
+            }}
+          >
+            {show ? "Hidden" : "Show"}
+          </span>
         </div>
 
         <button className="bg-[#0A66C2] text-white py-[10px] text-[18px] rounded-full focus:outline-none mt-5">
           Sign Up
         </button>
-        <p className="text-center" >Already have an account ? <span className="text-[#0A66C2] cursor-pointer" onClick={()=>navigate("/login")}>Sign In</span></p>
+        <p
+          onClick={() => navigate("/login")}
+          className="text-center curser cursor-pointer"
+        >
+          Already have an account ?{" "}
+          <span className="text-[#0A66C2] cursor-pointer">Sign In</span>
+        </p>
       </form>
     </div>
   );
